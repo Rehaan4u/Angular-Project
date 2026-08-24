9b5f7e92e1b9da7f27a2461073e4e69affa8898b
Rehaan Makhija <rehaanmakhija@gmail.com>
2026-08-24 18:07:51 +0530

    short summary for app.ts and app.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,3 +1,12 @@
+/*
+  this is like the app.component.ts 
+  here all the @Component and other decorators are declared
+  that provides two important things
+  --selector-- && --templateURL--
+  the selector tells angular ro look fro app-root tag in index.html
+  and templateURL points to HTML structure here it is app.html, that need to
+  be replaced with the app-root tag
+*/
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
