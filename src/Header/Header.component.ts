a77c4cb9138dac228f21a59f4c709d4ddff8ba43
Rehaan Makhija <rehaanmakhija@gmail.com>
2026-08-25 13:12:53 +0530

    Created the first Header.component.ts component and the my-file.txt for imp. concepts

---
+++ src/Header/Header.component.ts
@@ -0,0 +1,20 @@
+
+import {Component} from '@angular/core'
+
+@Component({
+    /* Very important to have two words seperated with -
+        like here it is app-header
+        cause if we keep it as header then it could possibly
+        collide with the actural <header> tag of html
+    */
+    selector: 'app-header',
+    templateUrl: './header.component.html' ,
+    /* standalone is a component flag, which means that this component
+        is completely independent and self-contianed and can be used anywhere
+        without the need to be imported and registered into the @NgModule
+        into the declaration array
+    */
+    standalone: true
+})
+//the class should be in export mode, so that other files can import it
+export class HeaderComponent {} 
--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,25 @@ import { bootstrapApplication } from '@angular/platform-browser';
      
      The Solution: By leaving the extension off, you let the bundler (like Vite or Esbuild in Angular) handle the resolution. 
      It looks for ./app/app.ts during development and accurately maps it to ./app/app.js in production.
+
+     //Angular is not automatically tracking your compnents, if you make one, tell the angular in the main.ts file
+
 */
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+//import the class from the constructor
+
+import {HeaderComponent} from './Header/Header.component';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+/*
+  Some knowledge about bootstrapApplication
+  bootstrapApplication is not a class. 
+  It is a standalone function imported from Angular's core 
+  library.
+
+  It's single job is to load the first root component usually 
+  the App inside the browsers DOM
+*/
+bootstrapApplication(HeaderComponent)
